398433ee670b4bc5f12753caf7f4e66eab2f3607
Brad Bryant <bradleybryant86@gmail.com>
2020-03-05 00:09:58 -0600

    Added last two assignments to Repos.js

--- react-portfolio/src/components/Project/index.js
+++ react-portfolio/src/components/Project/index.js
@@ -3,7 +3,12 @@ import React from "react";
 function Project () {
     return (
         <div>
-           {a href= https://danfenichel.github.io/Project1/}
+             <a href ="https://danfenichel.github.io/Project1/"> <p>Food and Brew Finder</p></a>
+             <a href ="https://brad-note-taker86.herokuapp.com/"> <p>Express Note Taker</p></a>
+             <a href ="https://b-bryant18.github.io/Homework-4-Code-Quiz/"> <p>The Mandalorian Quiz</p></a>
+             <a href ="https://glacial-journey-02223.herokuapp.com/"> <p>Fitness Tracker</p></a>
+             <a href ="https://b-bryant18.github.io/Homework-5/"> <p>Day Planner</p></a>
+             <a href ="https://prehab4all.herokuapp.com/"> <p>Prehab</p></a>
         </div>
 
     )
--- react-portfolio/src/pages/repos.js
+++ react-portfolio/src/pages/repos.js
@@ -1,14 +1,11 @@
 import React from "react";
 import Footer from "../components/Footer"
+import Project from "../components/Project";
 
 function Repos() {
     return (
         <div>
-             <a href ="https://danfenichel.github.io/Project1/"> <p>Food and Brew Finder</p></a>
-             <a href ="https://brad-note-taker86.herokuapp.com/"> <p>Express Note Taker App</p></a>
-             <a href ="https://b-bryant18.github.io/Homework-4-Code-Quiz/"> <p>The Mandalorian Quiz</p></a>
-             <a href ="https://glacial-journey-02223.herokuapp.com/"> <p>Fitness Tracker</p></a>
-             
+            <Project />
             <Footer />
         </div>
 
