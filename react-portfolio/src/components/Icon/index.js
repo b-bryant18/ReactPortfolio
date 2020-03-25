f3d20ea9d03abea892d2f1b91359249071c91d65
Brad Bryant <bradleybryant86@gmail.com>
2020-03-25 15:55:52 -0500

    Moved LinkedIn and pin images to the pages folder and imported them to the contact page

--- react-portfolio/src/components/Icon/index.js
+++ react-portfolio/src/components/Icon/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Link from "./linkedinIcon.png";
+import Pin from "./pin.png";
+import TX from "./TXoutline.png";
+import "./style.css";
 
 
 //Cannot call your function and an imported element the same name
 function Icon() {
     return (
-        <img src={Link} />
+        <div>
+        <img className="circle" src={Link} />
+        <img className="circle" src={Pin} />
+        <img className="circle" src={TX} />
+        </div>
     )
 };
 
--- react-portfolio/src/pages/contact.js
+++ react-portfolio/src/pages/contact.js
@@ -1,24 +1,31 @@
 import React from "react";
 import Container from "../components/Container";
 import Row from "../components/Row";
-import Icon from "../components/Icon";
+// import Icon from "../components/Icon";
+import pin from "./pin.png";
+import LinkedIn from "./linkedinIcon.png";
+import "./style.css";
 
 function Contact() {
     return (
         <div>
             <Container>
                 <Row>
-                    <h2>Location: Austin, TX </h2>
+                    <h2>
+                    <img className="circle" src = {pin} />
+                        Location: Austin, TX </h2>
                     <br />
                 </Row>
                 <Row>
-                    <h2>Email: bradleybryant86@gmail.com </h2>
+                    <h2>
+                        
+                        Email: bradleybryant86@gmail.com </h2>
                     <br />
                 </Row>
                 <Row>
                     <h1>
-                        <Icon />
-                    <a href="https://www.linkedin.com/in/bradley-bryant-194929148">LinkedIn</a>
+                     <img className="circle" src = {LinkedIn} />
+                        <a href="https://www.linkedin.com/in/bradley-bryant-194929148">LinkedIn</a>
                     </h1>
                     <br />
                 </Row>
